06cc85d51139a5333e8003d80ee86e1dd38e6974
jaewoo_oh <134061351+jaewoooh@users.noreply.github.com>
2025-07-07 19:07:59 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long a, b;
+
+    cin >> a >> b;
+
+    if ((a-b) < 0){
+        cout << -(a-b);
+    }
+    else
+        cout << (a-b);
+}
